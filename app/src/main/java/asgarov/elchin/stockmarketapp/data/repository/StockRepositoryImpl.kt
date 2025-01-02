ec37cf9283b9f630ba0310ea5a90502fb735f8af
AsgarovElchin <elcinaskerovki@gmail.com>
2025-01-02 14:29:46 +0400

    caching done

--- app/src/main/java/asgarov/elchin/stockmarketapp/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/asgarov/elchin/stockmarketapp/data/repository/StockRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package asgarov.elchin.stockmarketapp.data.repository
 
+import asgarov.elchin.stockmarketapp.data.csv.CSVParser
 import asgarov.elchin.stockmarketapp.data.local.StockDatabase
 import asgarov.elchin.stockmarketapp.data.mapper.toCompanyListing
+import asgarov.elchin.stockmarketapp.data.mapper.toCompanyListingEntity
 import asgarov.elchin.stockmarketapp.data.remote.StockApi
 import asgarov.elchin.stockmarketapp.domain.model.CompanyListing
 import asgarov.elchin.stockmarketapp.domain.repository.StockRepository
@@ -12,12 +14,13 @@ import okio.IOException
 import retrofit2.HttpException
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.math.truncate
+
 
 @Singleton
 class StockRepositoryImpl @Inject constructor(
     val api: StockApi,
     val db: StockDatabase,
+    val companyListingParser: CSVParser<CompanyListing>
 ):StockRepository {
 
     private val dao = db.dao
@@ -43,14 +46,25 @@ class StockRepositoryImpl @Inject constructor(
 
             val remoteListings = try {
                 val response = api.getListings()
-                response.byteStream()
-
+                companyListingParser.parse(response.byteStream())
             }catch (e:IOException){
                 e.printStackTrace()
                 emit(Resource.Error("Couldn't load data"))
+                null
             }catch (e:HttpException){
                 e.printStackTrace()
                 emit(Resource.Error("Couldn't load data"))
+                null
+            }
+
+            remoteListings?.let { listings->
+                dao.clearCompanyListings()
+                dao.insertCompanyListings(listings.map { it.toCompanyListingEntity() })
+                emit(Resource.Success(data = dao.searchCompanyListing("").map {
+                    it.toCompanyListing()
+                }))
+                emit(Resource.Loading(false))
+
             }
         }
     }
